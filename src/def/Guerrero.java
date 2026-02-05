4511b26fdc86f616b80ec669d3f841948e04d789
JordiCoso <jcosopa@alumnes.ilerna.com>
2026-02-05 18:14:26 +0100

    Funcion proteger

--- src/def/Guerrero.java
+++ src/def/Guerrero.java
@@ -1,12 +1,25 @@
 package def;
 
 public class Guerrero extends Personaje {
-	
+
 	Personaje companeroProtegido;
 
 	public Guerrero(String nombre, int nivel, int puntosVida, boolean protegido) {
 		super(nombre, nivel, puntosVida, protegido);
 		this.companeroProtegido = null;
+	}
+
+	void proteger(Personaje companero) {
+
+		if (this.companeroProtegido == null) {
+			
+			this.companeroProtegido = companero;
+			
+		} else {
+
+			System.out.println("Imposible proteger. Ya estas protegiendo a otro compañero");
+
+		}
 	}
 
 }
