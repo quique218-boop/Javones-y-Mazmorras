f16f2bd9535906c9807aaacb37473ec4b2611a77
JordiCoso <jcosopa@alumnes.ilerna.com>
2026-02-05 18:29:53 +0100

    creacion robar

--- src/def/Ladron.java
+++ src/def/Ladron.java
@@ -1,7 +1,7 @@
 package def;
 
 public class Ladron extends Personaje {
-	
+
 	boolean invisible;
 
 	public Ladron(String nombre, int nivel, int puntosVida, boolean protegido) {
@@ -9,4 +9,10 @@ public class Ladron extends Personaje {
 		this.invisible = false;
 	}
 
+	public void robar() {
+		
+		String resultadoRobo = invisible ? "No te han visto robar al ser invisible" : "Te han pillado robando por ser visible";
+		
+		System.out.println(resultadoRobo);
+	}
 }
