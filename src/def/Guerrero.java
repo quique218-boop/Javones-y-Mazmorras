f5419bdb10a82f9f35e7a60fc9096ed2e402ed83
JordiCoso <jcosopa@alumnes.ilerna.com>
2026-02-05 18:11:28 +0100

    Constructor Guerrero

--- src/def/Guerrero.java
+++ src/def/Guerrero.java
@@ -1,9 +1,12 @@
 package def;
 
 public class Guerrero extends Personaje {
+	
+	Personaje companeroProtegido;
 
 	public Guerrero(String nombre, int nivel, int puntosVida, boolean protegido) {
 		super(nombre, nivel, puntosVida, protegido);
+		this.companeroProtegido = null;
 	}
 
 }
