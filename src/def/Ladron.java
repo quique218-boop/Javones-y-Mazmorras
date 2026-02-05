cb4ca206c465152e285362c55fa6fa5015744a76
JordiCoso <jcosopa@alumnes.ilerna.com>
2026-02-05 18:35:54 +0100

    Comentarios

--- src/def/Ladron.java
+++ src/def/Ladron.java
@@ -4,10 +4,14 @@ public class Ladron extends Personaje {
 
 	boolean invisible;
 
+	// Constructor usa las mismas variables que personaje y pone invisible a falso.
+	
 	public Ladron(String nombre, int nivel, int puntosVida, boolean protegido) {
 		super(nombre, nivel, puntosVida, protegido);
 		this.invisible = false;
 	}
+	
+	// Si esta invisible devuelve el primer texto, de lo contrario, el segundo.
 
 	public void robar() {
 
@@ -15,11 +19,15 @@ public class Ladron extends Personaje {
 
 		System.out.println(resultadoRobo);
 	}
+	
+	// Invierte el valor actual de invisible. Si es falso ==> verdadero. Si es verdadero ==> falso.
 
 	void hacerseInvisible() {
 		this.invisible = this.invisible ? false : true;
 	}
 
+	// Devuelve el valor actual de invisible.
+	
 	boolean estaInvisible() {
 		return this.invisible;
 	}
