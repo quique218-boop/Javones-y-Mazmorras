6258387bbdb33998c5ac8f86d5785e046f3ee0e5
joseporiolse-creator <joseporiolse@alumnes.ilerna.com>
2026-02-06 16:11:54 +0100

    avance main

--- src/def/Main.java
+++ src/def/Main.java
@@ -6,25 +6,39 @@ public class Main {
 	public static void main(String[] args) {
 
 		Scanner scan = new Scanner(System.in);
-		
+
 		System.out.println("Bienvenidos a JAVONES Y MAZMORRAS");
-		
-		System.out.println("Como te llamas aventurero?");
-		
-		String nombre = scan.nextLine();
-		
-		System.out.println("Que clase eres? \n Guerrero \n Mago \n Ladron");
-		
-		String clase = scan.nextLine();
-		
-		switch(clase) {
-		
-		case "guerrero":
+
+		System.out.println("Que tipo de aventura quieres? ");
+
+		System.out.println("PARTIDA POR DEFECTO");
+
+		System.out.println("PARTIDA PERSONALIZADA");
+
+		String opcion = scan.nextLine();
+
+		switch(opcion) {
+
+		case "partida por defecto":
+
+			System.out.println("Como te llamas aventurero?");
+
+			String nombre = scan.nextLine();
+
+			System.out.println("Que clase eres? \n Guerrero \n Mago \n Ladron");
+
+			String clase = scan.nextLine();
 			
 			
+
+
 		}
-		
-		
+
+
+
+
+
+
 	}
 
 }
